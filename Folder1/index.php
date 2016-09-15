94ce4b127942537107bbd879cc43ca90c01a7c39
user <user@192.168.1.4>
2016-09-15 17:33:36 +0200

    chahiane

--- Folder1/index.php
+++ Folder1/index.php
@@ -1,3 +1,30 @@
-<?php
-
-echo 'Hello Word ';
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  
+    <title>Imagehover.css - An Image Hover CSS Library</title>
+    <link href="css/demo-page.css" rel="stylesheet" media="all">
+    <link href="css/imagehover.css" rel="stylesheet" media="all">
+    </head>
+  <body>
+   <div class="demo">
+          <figure class="imghvr-push-up"><img src="example-image.jpg" alt="example-image">
+            <figcaption>
+              <h3>Hello World</h3>
+              <p>Life is too important to be taken seriously!</p>
+            </figcaption><a href="javascript:;"></a>
+          </figure>
+          <textarea onclick="this.focus();this.select()" readonly="readonly">imghvr-push-up</textarea>
+        </div>
+      <div class="demo">
+          <figure class="imghvr-fade"><img src="example-image.jpg" alt="example-image">
+            <figcaption>
+              <h3>Hello World</h3>
+              <p>Life is too important to be taken seriously!</p>
+            </figcaption><a href="javascript:;"></a>
+          </figure>
+          <textarea onclick="this.focus();this.select()" readonly="readonly">imghvr-fade</textarea>
+        </div>
+  </body>
+</html>
